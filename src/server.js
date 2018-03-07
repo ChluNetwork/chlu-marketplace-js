8fa09f63c7a5ab33b9fea00db7a2c6bad0de27b4
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-03-07 14:50:37 +0100

    add CORS, fix error handling in http

--- src/server.js
+++ src/server.js
@@ -1,37 +1,49 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const Marketplace = require('./marketplace');
 
 const app = express();
 app.locals.mkt = new Marketplace();
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Chlu Marketplace'));
 
 app.get('/vendors', async (req, res) => {
-    res.json(await app.locals.mkt.getVendorIDs());
+    await respond(res, app.locals.mkt.getVendorIDs());
 });
 app.post('/vendors', async (req, res) => {
     const pubKeyMultihash = req.body.vendorPubKeyMultihash;
-    res.json(await app.locals.mkt.registerVendor(pubKeyMultihash)); 
+    await respond(res, app.locals.mkt.registerVendor(pubKeyMultihash)); 
 });
 app.post('/vendors/:id/signature', async (req, res) => {
     const signature = req.body.signature;
-    res.json(await app.locals.mkt.updateVendorSignature(req.params.id, signature)); 
+    await respond(res, app.locals.mkt.updateVendorSignature(req.params.id, signature)); 
 });
 app.get('/vendors/:id', async (req, res) => {
-    res.json(await app.locals.mkt.getVendor(req.params.id));
+    respond(res, app.locals.mkt.getVendor(req.params.id));
 });
 
 app.post('/vendors/:id/popr', async (req, res) => {
-    res.json(await app.locals.mkt.createPoPR(req.params.id, req.body || {}));
+    await respond(res, app.locals.mkt.createPoPR(req.params.id, req.body || {}));
 });
 
 app.get('/.well-known', async (req, res) => {
     const keys = await app.locals.mkt.getKeys();
-    res.json({
+    await respond(res, {
         multihash: keys.pubKeyMultihash
     });
 });
+
+async function respond(res, promise) {
+    try {
+        const data = await promise;
+        res.json(data);
+        return data;
+    } catch (err) {
+        res.status(500).send(err.message || err);
+    }
+}
 
 module.exports = app;
